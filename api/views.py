4c82f380462d972ceca7271dcab2e6ffe0f8b582
rameshdjango <rameshpython1224@gmail.com>
2019-12-30 13:10:31 +0530

    added jwt authentication

---
+++ api/views.py
@@ -0,0 +1,16 @@
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
+
+    def post(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
